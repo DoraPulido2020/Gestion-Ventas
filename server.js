d3006371d5adf8ec151959c7c11cc28169089b4b
douglasd11 <dguerreroe@unicartagena.edu.co>
2023-11-02 16:51:34 -0500

    cors

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors({
-    origin: "https://gestor-ventas-xb5v.onrender.com/"
+    origin: "https://gestor-ventas-xb5v.onrender.com"
 }))
 
 //import conexion mongoDB
