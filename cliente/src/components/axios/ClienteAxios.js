08d645ff95c15b91c05be23fd54d2f24bb2d2fa6
douglasd11 <dguerreroe@unicartagena.edu.co>
2023-11-02 16:34:52 -0500

    cambios en axios

--- cliente/src/components/axios/ClienteAxios.js
+++ cliente/src/components/axios/ClienteAxios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const clienteAxios = axios.create({
     
-    baseURL : 'http://localhost:3001/api/'
+    baseURL : 'https://api-gestor-ventas.onrender.com/api/'
 });
 
 export default clienteAxios;
